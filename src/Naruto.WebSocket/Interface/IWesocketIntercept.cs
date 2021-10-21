e6d282c2a64d7a81964874c41592f2c460d57f14
也许所谓 <1091675746@qq.com>
2021-10-21 23:01:41 +0800

    更新拦截器

---
+++ src/Naruto.WebSocket/Interface/IWesocketIntercept.cs
@@ -0,0 +1,32 @@
+﻿using Naruto.WebSocket.Object;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naruto.WebSocket.Interface
+{
+    /// <summary>
+    /// websocket拦截器
+    /// </summary>
+    public interface IWesocketIntercept
+    {
+        /// <summary>
+        /// 上线通知
+        /// </summary>
+        /// <returns></returns>
+        Task OnLineAsync(WebSocketClient webSocketClient);
+        /// <summary>
+        /// 下线通知
+        /// </summary>
+        /// <returns></returns>
+        Task OffLineAsync(WebSocketClient webSocketClient);
+        /// <summary>
+        /// 消息接收
+        /// </summary>
+        /// <param name="webSocketClient"></param>
+        /// <param name="webSocketMessage"></param>
+        /// <returns></returns>
+        Task ReciveAsync(WebSocketClient webSocketClient, WebSocketMessageModel webSocketMessage);
+    }
+}
