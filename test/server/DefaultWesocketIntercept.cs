a4a6cd8dc7f0470fec2dd885203eb36e277a76e1
张海波 <zhanghaibo@zhanghaibodeMacBook-Pro.local>
2022-03-06 18:49:02 +0800

    提交代码优化

---
+++ test/server/DefaultWesocketIntercept.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Naruto.WebSocket.Interface;
+using Naruto.WebSocket.Object;
+
+namespace server
+{
+	public class DefaultWesocketIntercept: IWesocketIntercept
+	{
+        private readonly ILogger<DefaultWesocketIntercept> _logger;
+		public DefaultWesocketIntercept(ILogger<DefaultWesocketIntercept> logger)
+		{
+            _logger = logger;
+		}
+
+        public ValueTask OffLineAsync(WebSocketClient webSocketClient)
+        {
+            _logger.LogInformation("连接下线，ConnectionId={ConnectionId}",webSocketClient.ConnectionId);
+            return new ValueTask();
+        }
+
+        public ValueTask OnLineAsync(WebSocketClient webSocketClient)
+        {
+            _logger.LogInformation("上线通知，ConnectionId={ConnectionId}", webSocketClient.ConnectionId);
+            return new ValueTask();
+        }
+
+        public ValueTask ReciveAsync(WebSocketClient webSocketClient, WebSocketMessageModel webSocketMessage)
+        {
+            _logger.LogInformation("消息接收，ConnectionId={ConnectionId}", webSocketClient.ConnectionId);
+            return new ValueTask();
+        }
+    }
+}
+
